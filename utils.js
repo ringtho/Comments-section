d1fc38942ff39fc309fc46cd8fd9279d9ddf06fc
Smith Ringtho <sringtho@gmail.com>
2023-01-21 13:06:00 +0300

    Added display for replies

--- utils.js
+++ utils.js
@@ -29,7 +29,44 @@ function getPostHtml(comment){
             </div>
         </section>
     `
-    return post
+    let replyHtml = ""
+    if (comment.replies.length){
+        comment.replies.forEach(reply => {
+            const replies = `
+            <section class="section-replies">
+                <section class="section comments">
+                    <div class="comments-container">
+                        <div class="section-header">
+                            <img class="section-avatar" src=${reply.user.image.png} />
+                            <p class="section-username">${reply.user.username}</p>
+                            <p class="section-time">${reply.createdAt}</p>
+                            <div class="section-reply-btn large-screen">
+                                <img class="reply-icon" src="/images/icon-reply.svg" />
+                                <span class="btn-text">Reply</span>
+                            </div>
+                        </div>
+                        <p class="section-comment"><span class="reply-username">@${reply.replyingTo}</span> ${reply.content}</p>
+                    </div>
+                    <div class="section-btns">
+                        <div class="section-add-remove-btn">
+                            <img class="plus-icon" src="images/icon-plus.svg" />
+                            <p class="btn-text">${reply.score}</p>
+                            <img class="minus-icon" src="images/icon-minus.svg" />
+                        </div>
+                        <div class="section-reply-btn small-screen">
+                            <img class="reply-icon" src="/images/icon-reply.svg" />
+                            <span class="btn-text">Reply</span>
+                        </div>
+                    </div>
+                </section>
+		    </section>
+            `
+            replyHtml += replies
+            
+        })
+    }
+    const html = post + replyHtml
+    return html
 }
 
 function getAddNewCommentHtml(data){
