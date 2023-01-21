3d527aa1bb4401a68d7158c5d8d9fffc7555396e
Smith Ringtho <sringtho@gmail.com>
2023-01-21 13:36:11 +0300

    Separated replies html from post html for ease during maintenance

--- utils.js
+++ utils.js
@@ -1,4 +1,4 @@
-
+import data from "./data.json" assert {type: "json"}
 
 function getPostHtml(comment){
     const post = `
@@ -31,42 +31,72 @@ function getPostHtml(comment){
     `
     let replyHtml = ""
     if (comment.replies.length){
-        comment.replies.forEach(reply => {
-            const replies = `
-            <section class="section-replies">
-                <section class="section comments">
-                    <div class="comments-container">
-                        <div class="section-header">
-                            <img class="section-avatar" src=${reply.user.image.png} />
-                            <p class="section-username">${reply.user.username}</p>
-                            <p class="section-time">${reply.createdAt}</p>
-                            <div class="section-reply-btn large-screen">
-                                <img class="reply-icon" src="/images/icon-reply.svg" />
-                                <span class="btn-text">Reply</span>
-                            </div>
-                        </div>
-                        <p class="section-comment"><span class="reply-username">@${reply.replyingTo}</span> ${reply.content}</p>
-                    </div>
-                    <div class="section-btns">
-                        <div class="section-add-remove-btn">
-                            <img class="plus-icon" src="images/icon-plus.svg" />
-                            <p class="btn-text">${reply.score}</p>
-                            <img class="minus-icon" src="images/icon-minus.svg" />
-                        </div>
-                        <div class="section-reply-btn small-screen">
-                            <img class="reply-icon" src="/images/icon-reply.svg" />
-                            <span class="btn-text">Reply</span>
-                        </div>
-                    </div>
-                </section>
-		    </section>
-            `
-            replyHtml += replies
-            
-        })
+        replyHtml += getReplyHtml(comment.replies)
     }
     const html = post + replyHtml
     return html
+}
+
+function getReplyHtml(replyArr){
+    let replyHtml = ""
+    replyArr.forEach(reply => {
+        let isCurrentUser = reply.user.username === data.currentUser.username
+        const replies = `
+        <section class="section-replies">
+            <section class="section comments">
+                <div class="comments-container">
+                    <div class="section-header">
+                        <img class="section-avatar" src=${reply.user.image.png} />
+                        <p class="section-username">${reply.user.username}</p>
+                        ${isCurrentUser ? `<span class="you-tag">You</span>`: ""}
+                        <p class="section-time">${reply.createdAt}</p>
+                        ${isCurrentUser ? 
+                        `<div class="delete-edit-btns">
+                            <div class="section-reply-btn mg large-screen">
+                                <img class="delete-icon" src="/images/icon-delete.svg" />
+                                <span class="btn-text red">Delete</span>
+                            </div>
+                            <div class="section-reply-btn large-screen">
+                                <img class="edit-icon" src="/images/icon-edit.svg" />
+                                <span class="btn-text">Edit</span>
+                        </div>
+                        </div>` :
+                        `<div class="section-reply-btn large-screen">
+                            <img class="reply-icon" src="/images/icon-reply.svg" />
+                            <span class="btn-text">Reply</span>
+                        </div>`
+                        }
+                    </div>
+                    <p class="section-comment"><span class="reply-username">@${reply.replyingTo}</span> ${reply.content}</p>
+                </div>
+                <div class="section-btns">
+                    <div class="section-add-remove-btn">
+                        <img class="plus-icon" src="images/icon-plus.svg" />
+                        <p class="btn-text">${reply.score}</p>
+                        <img class="minus-icon" src="images/icon-minus.svg" />
+                    </div>
+                    ${isCurrentUser ?
+                    `<div class="section-reply-btn mg small-screen">
+                    <img class="delete-icon" src="/images/icon-delete.svg" />
+                    <span class="btn-text red">Delete</span>
+                    </div>
+                    <div class="section-reply-btn small-screen">
+                        <img class="edit-icon" src="/images/icon-edit.svg" />
+                        <span class="btn-text">Edit</span>
+                    </div> `:
+                    `<div class="section-reply-btn small-screen">
+                        <img class="reply-icon" src="/images/icon-reply.svg" />
+                        <span class="btn-text">Reply</span>
+                    </div>`
+                    }
+                </div>
+            </section>
+        </section>
+        `
+        replyHtml += replies
+
+    })
+    return replyHtml
 }
 
 function getAddNewCommentHtml(data){
